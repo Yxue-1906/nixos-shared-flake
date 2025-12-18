69915f6a2acf092ab525674e9ea0f8e433c90227
nanashi <na@na.shi>
2025-12-18 20:10:34 +0800

    feat: upgrade nixpkgs to 25.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "nanashi's Nixos shared flakes";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/6faeb062ee4cf4f105989d490831713cc5a43ee1";
+    nixpkgs.url = "github:NixOS/nixpkgs/c6f52ebd45e5925c188d1a20119978aa4ffd5ef6";
   };
   outputs = { nixpkgs, ... }@inputs: with nixpkgs.lib; {
     inherit nixpkgs;
