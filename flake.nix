9f2bdd49079817503e93d7ccf59cafd821789a56
nanashi <na@na.shi>
2026-06-04 18:48:43 +0800

    deps: upgrade nixkpgs to 26.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "nanashi's Nixos shared flakes";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/71caefce12ba78d84fe618cf61644dce01cf3a96";
+    nixpkgs.url = "github:NixOS/nixpkgs/6b316287bae2ee04c9b93c8c858d930fd07d7338";
   };
   outputs = { nixpkgs, ... }@inputs: with nixpkgs.lib; {
     inherit nixpkgs;
