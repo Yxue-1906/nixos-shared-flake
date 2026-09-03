e88c0b67c103c1663f3220de7d5de087c1c1606f
nanashi <na@na.shi>
2026-09-03 23:57:38 +0800

    deps: update nixpkgs version

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "nanashi's Nixos shared flakes";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/6b316287bae2ee04c9b93c8c858d930fd07d7338";
+    nixpkgs.url = "github:NixOS/nixpkgs/3ed67ec0a4d3c7ab4ae1f04f8ee8df07bfa506a2";
   };
   outputs = { nixpkgs, ... }@inputs: with nixpkgs.lib; {
     inherit nixpkgs;
